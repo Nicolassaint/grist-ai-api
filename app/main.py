a6a422e465a63c25abf3c63d31b9be117e90d134
goddyduch <nicolas.saint78@gmail.com>
2025-06-03 14:18:52 +0200

    feat: agents

--- app/main.py
+++ app/main.py
@@ -274,7 +274,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app", 
         host="0.0.0.0", 
-        port=8000, 
+        port=8502, 
         reload=True,
         log_level="info"
     ) 
